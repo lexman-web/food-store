bfc6c5c53c28784e2f05d3f393cf83bf39d07908
Alexander Lexman <axx.semeniaka@gmail.com>
2021-10-19 09:01:20 +0300

    finish page markup

--- js/main.js
+++ js/main.js
@@ -1,5 +1,42 @@
-function  startPage(comment) {
-	console.log(comment);
-};
+"use strict";
 
-startPage();
+/* running functions */
+addBurger();
+
+
+
+
+
+
+/* burger menu */
+function addBurger() {
+	const burgerButton = document.querySelector(".menu-button"),
+		burgerMenu = document.querySelector(".menu"),
+		burgerLines = document.querySelectorAll(".menu-button__line");
+
+	if (burgerButton) {
+		burgerButton.addEventListener("click", () => {
+			burgerButton.classList.toggle("menu-button--active");
+			burgerMenu.classList.toggle("menu--open");
+			document.body.classList.toggle("body--lock");
+
+			for (let i = 0; i < burgerLines.length; i++) {
+				burgerLines[i].classList.toggle("menu-button__line--active");
+			}
+		})
+	};
+
+	// hover for line
+	burgerButton.addEventListener("mouseover", () => {
+		burgerLines.forEach(element => {
+			element.classList.add("menu-button__line--over")
+		});
+	});
+
+	burgerButton.addEventListener("mouseout", () => {
+		burgerLines.forEach(element => {
+			element.classList.remove("menu-button__line--over")
+		});
+	});
+
+};
